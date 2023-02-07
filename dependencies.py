938f5e125bf7c5418dff9204254105d5a127fd20
Sergey Krasyuk <takecard42@mail.ru>
2023-02-07 20:01:08 +0700

    rename MenuDAL

--- dependencies.py
+++ dependencies.py
@@ -1,8 +1,8 @@
 from db.config import async_session
-from db.dals.book_dal import BookDAL
+from db.dals.book_dal import MenuDAL
 
 
 async def get_book_dal():
     async with async_session() as session:
         async with session.begin():
-            yield BookDAL(session)
+            yield MenuDAL(session)
